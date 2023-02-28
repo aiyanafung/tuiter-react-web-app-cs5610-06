a2f78770bfcd7d21795824c14c10aabdc459460b
Yazhe Feng <yazhe.fyz@gmail.com>
2023-02-28 11:09:54 -0800

    fix bug of imgs

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -8,7 +8,7 @@ function Tuiter() {
  return (
         <div className="row mt-2">
                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                   <NavigationSidebar/>
+                   <NavigationSidebar active="explore"/>
                    <button className="btn btn-primary w-100 mt-2 rounded-pill">Tuit</button>
                </div>
                <div className="col-10 col-md-10 col-lg-7 col-xl-6"
